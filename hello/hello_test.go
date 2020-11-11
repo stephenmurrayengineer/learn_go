922a976898fd3c6b68d9a44024f5a3666d95937b
stephenmurrayengineer <stephenmurrayengineer@gmail.com>
2020-11-11 16:53:29 +0000

    Include test case where empty string is taken by Hello()

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -5,13 +5,28 @@ import "testing"
 
 // write a test function here for each function you have written
 func TestHello(t *testing.T) {
-	// declaring some vars:
-	got := Hello("Chris")
-	want := "Hello, Chris"
 
-	// if statements and string formatting are pretty standard:
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	// refactor tests into an assertion to remove duplication
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		// helper func displays line func is called:
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	// test case where there is an arg to hello function:
+	t.Run("saying hello to people", func(t *testing.T){
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	// test case where there is no arg
+	t.Run("say hello world, when func takes an empty string", func(t *testing.T){
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
