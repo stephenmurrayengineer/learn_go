d89bf889e1ea2ae42593e450d21814a8281b6183
stephenmurrayengineer <stephenmurrayengineer@gmail.com>
2020-11-11 17:13:37 +0000

    Adds Spanish Language Option in Hello

--- hello/hello.go
+++ hello/hello.go
@@ -6,13 +6,16 @@ import "fmt"
 // constanst are defined like this:
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == ""{
 		name = "World"
+	}
+	if language == "Spanish"{
+		return "Hola, " + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("World", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -17,15 +17,22 @@ func TestHello(t *testing.T) {
 
 	// test case where there is an arg to hello function:
 	t.Run("saying hello to people", func(t *testing.T){
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	// test case where there is no arg
 	t.Run("say hello world, when func takes an empty string", func(t *testing.T){
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	// test case for spanish
+	t.Run("say hello world in spanish", func(t *testing.T){
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
