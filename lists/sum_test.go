349204002f268c580532e549c39bb5bca3572762
stephenmurrayengineer <stephenmurrayengineer@gmail.com>
2020-11-13 16:03:35 +0000

    modify tests to account for empty slices

--- lists/sum_test.go
+++ lists/sum_test.go
@@ -28,3 +28,26 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("got %d want %d given",got, want)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+	//helper function to check lists:
+	checkSums := func(t * testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of tails of slices n=2", func(t *testing.T) {
+		got := SumAllTails([]int{1,2}, []int{0,9})
+		want := []int{2,9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("make the sums of tails of empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3,4,5})
+		want := []int{0,9}
+		checkSums(t, got, want)
+	})
+
+}
