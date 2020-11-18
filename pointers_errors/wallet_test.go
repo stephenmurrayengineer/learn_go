8945f9d6eb1bfab4788990771b7e375cd826ba91
stephenmurrayengineer <stephenmurrayengineer@gmail.com>
2020-11-18 17:54:18 +0000

    refactor Wallet tests to remove code duplication

--- pointers_errors/wallet_test.go
+++ pointers_errors/wallet_test.go
@@ -5,17 +5,22 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}		
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
 	
-		got := wallet.Balance()
-		want := Bitcoin(10)
-	
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
@@ -23,12 +28,7 @@ func TestWallet(t *testing.T) {
 
 		wallet.Withdraw(Bitcoin(10))
 	
-		got := wallet.Balance()
-		want := Bitcoin(10)
-	
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 }
