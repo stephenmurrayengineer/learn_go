762f88a7ab25e8b543def3d50ca363d48ab4ee12
stephenmurrayengineer <stephenmurrayengineer@gmail.com>
2020-11-18 17:39:08 +0000

    correctly use pointers to Wallet in Wallet methods

--- pointers_errors/wallet.go
+++ pointers_errors/wallet.go
@@ -1,13 +1,17 @@
 package main
 
 type Wallet struct {
-	
+	balance int	
 }
 
-func (w Wallet) Deposit(amount int) {
 
+// when referencing Wallet in these functions, a copy is made.
+// Therefore, use a pointer
+func (w *Wallet) Deposit(amount int) {
+	// notice that you don't need to dereference the pointer:
+	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
-	return 0
+func (w *Wallet) Balance() int {
+	return w.balance
 }
