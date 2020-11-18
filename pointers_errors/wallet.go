caa3362f5a0d609804ceedb7e30f87e63cbf49d0
stephenmurrayengineer <stephenmurrayengineer@gmail.com>
2020-11-18 17:45:09 +0000

    includes int type Bitcoin for use with Wallet{}

--- pointers_errors/wallet.go
+++ pointers_errors/wallet.go
@@ -1,17 +1,28 @@
 package main
 
-type Wallet struct {
-	balance int	
+import "fmt"
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
 }
 
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
 
 // when referencing Wallet in these functions, a copy is made.
 // Therefore, use a pointer
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	// notice that you don't need to dereference the pointer:
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
