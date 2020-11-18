ab151327a467674e794ea9fedf4ac797c4daf3dd
stephenmurrayengineer <stephenmurrayengineer@gmail.com>
2020-11-18 16:54:26 +0000

    refactor area tests on shapes into a single struct array

--- structs_methods/shapes_test.go
+++ structs_methods/shapes_test.go
@@ -2,40 +2,22 @@ package main
 
 import "testing"
 
-// func TestPerimeter(t *testing.T) {
-// 	rectangle := Rectangle{10.0, 10.0}
-// 	got := Perimeter(rectangle)
-// 	want := 40.0
-
-// 	if got != want {
-// 		t.Errorf("got %.2f want %.2f", got, want)
-// 	}
-// }
-
-
-
 func TestArea(t *testing.T) {
 
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	// slice of structs to iterate over:
+	areaTests := []struct {
+			shape Shape
+			want float64
+	}{
+		{Rectangle{12,6}, 72},
+		{Circle{10}, 314.1592653589793},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		want := 72.0
-
-		checkArea(t, rectangle, want)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		want := 314.1592653589793
-
-		checkArea(t, circle, want)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
 
 }
